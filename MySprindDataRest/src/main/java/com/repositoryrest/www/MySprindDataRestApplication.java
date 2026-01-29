9fd51f5220216b7bd185246051feae6473451423
aadi <aadisaagara@gmail.com>
2026-01-29 16:40:26 +0530

    changes from local

--- MySprindDataRest/src/main/java/com/repositoryrest/www/MySprindDataRestApplication.java
+++ MySprindDataRest/src/main/java/com/repositoryrest/www/MySprindDataRestApplication.java
@@ -9,6 +9,8 @@ public class MySprindDataRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MySprindDataRestApplication.class, args);
                 int a=100;
+                
+                double d=125.5;
 	}
 
 }
