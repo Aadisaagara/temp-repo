c58b59c1609e8de0af0813285bd95ddae5f9c951
aadi <aadisaagara@gmail.com>
2026-01-29 15:58:16 +0530

    changes made in boot main app

--- MySprindDataRest/src/main/java/com/repositoryrest/www/MySprindDataRestApplication.java
+++ MySprindDataRest/src/main/java/com/repositoryrest/www/MySprindDataRestApplication.java
@@ -8,6 +8,7 @@ public class MySprindDataRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySprindDataRestApplication.class, args);
+                int a=100;
 	}
 
 }
